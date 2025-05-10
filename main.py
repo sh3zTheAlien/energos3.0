0a6a891530f5cb67ff9d65ad846e5380432aa301
sh3zTheAlien <ioannismrgl23@gmail.com>
2025-05-10 19:33:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import json
 from flask import Flask, render_template
 from datetime import datetime as dt
-#remove "/" in path
 
 app = Flask(__name__)
-with open('data.json','r',encoding='UTF-8') as data_file: #replace with actual path in server
+with open('data.json','r',encoding='UTF-8') as data_file:
     locations_file = json.load(data_file)
 
 @app.route('/')
@@ -18,7 +17,3 @@ def locations():
 @app.route('/locations/post-<location_id>/<name>')
 def destination(name,location_id):
     return render_template('destination.html', year=str(dt.now().year), location=int(location_id), locations=locations_file)
-
-#remove it in sever!, It handles it on its own
-if __name__ == '__main__':
-    app.run(debug=True,use_reloader=True)
